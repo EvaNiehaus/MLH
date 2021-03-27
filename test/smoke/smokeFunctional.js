bcaf02d9bbdee0af503ff41abaaa19b780b949e6
Eva Niehaus <vl1vl@yahoo.com>
2021-03-27 20:52:09 +0100

    updated

--- test/smoke/smokeFunctional.js
+++ test/smoke/smokeFunctional.js
@@ -1,5 +1,5 @@
 import sel from '../../data/selectors';
-
+import {name, gender, age} from '../../data/testData';
 describe('Required fields and story created', function () {
 
     before('Open App', function () {
@@ -7,11 +7,11 @@ describe('Required fields and story created', function () {
     });
 
     it('TC-026 Submit button is enabled after fields 1-4 are filled in with valid values', function () {
-        $(sel.name).setValue('LadyBug007');
-        $$(sel.radioButtons)[1].click();
-        $(sel.age).setValue('1234567890');
+        $(sel.name).setValue(name.default);
+        $$(sel.radioButtons)[gender.she].click();
+        $(sel.age).setValue(age.default);
         $(sel.storyType).click();
-        $$(sel.storyList)[6].click();
+        $$(sel.storyList)[story.comedy].click();
         let submitBtn = $(sel.submit).isEnabled();
         expect(submitBtn).toEqual(true);
     });
